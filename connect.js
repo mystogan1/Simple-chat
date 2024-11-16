a5ce903d07fa719ced32065d36a6dc3b809a525a
Mystogan1 <shashikantsai46@gmail.com>
2024-11-16 19:03:17 +0530

    Added the online element to see users count

--- connect.js
+++ connect.js
@@ -3,6 +3,7 @@ console.log('test...');
 const messageInput = document.getElementById('messageInput');
 const sendButton = document.getElementById('sendButton');
 const messageDisplay = document.getElementById('messageDisplay');
+const onlineUsers = document.getElementById("onlineUsers");
 
 // Optional: Send message on pressing Enter key
 messageInput.addEventListener('keydown', (e) => {
@@ -16,29 +17,37 @@ const socket = new WebSocket("wss://my-websocket-sasi.glitch.me/");
 
 // Connection opened
 socket.addEventListener("open", (event) => {
-  messageDisplay.innerHTML = 'You will not see past messages';
+    messageDisplay.innerHTML = 'You will not see past messages';
+
   sendButton.addEventListener('click', () => {
     const message = messageInput.value.trim();
 
     if(message){
-        const payload = {sender: 'human', content:message}
+        const payload = {type: 'message', user: 'Human', content:message, }
         socket.send(JSON.stringify(payload));
     }
   })
-  
 
-});
+
+})
 
 // Listen for messages
 socket.addEventListener("message", (event) => {
   console.log("Message from server ", event.data);
-//   console.log("Message from server ", event);
 
+  const data = JSON.parse(event.data);
+  if(data.type == 'userCount'){
+      onlineUsers.innerHTML = "Online: " + data.count;
+  }
+  
   const messageElement = document.createElement("div");
   messageElement.classList.add("message");
-  messageElement.innerHTML =
-    JSON.parse(event.data).sender + ": " + JSON.parse(event.data).content;
-  messageDisplay.appendChild(messageElement);
-  messageInput.value = "";
-  messageDisplay.scrollTop = messageDisplay.scrollHeight; // Auto-scroll to the bottom
+  if(data.type=='message'){
+      messageElement.innerHTML =
+        JSON.parse(event.data).user + ": " + JSON.parse(event.data).content;
+      messageDisplay.appendChild(messageElement);
+      messageInput.value = "";
+      messageDisplay.scrollTop = messageDisplay.scrollHeight; // Auto-scroll to the bottom
+  }
 });
+
